f3b78014d29b9f58a739b5b2b1910b794b4624f8
matilde tun chi <matildetunchi@gmail.com>
2017-03-28 14:36:28 -0600

    se cargaron los archivos

--- progra_haskell.hs
+++ progra_haskell.hs
@@ -2,16 +2,19 @@
 mediaNumeros x y z = (x+y+z)/3
 
 -- 2 Volumen de una esfera
-volEsfera v = (4/3)*pi*v^3
+volEsfera r = (4/3)*pi*r^3
 
 -- 3 Numeros impares empezando desde el numero 13
-impTrece = [13,15..29]
+impTrece = [13,15..33]
 
 -- 4 Determinar el numero mayor entre 3 numeros
 maxiNum x y z = max x (max y z)
 
 -- 5 Rotar una lista de numeros
-rota pos list = drop pos list ++ take pos list
+--rota pos list = drop pos list ++ take pos list
+rota1 x =  la cola x ++ [ cabeza x]
+rota nx =  caída nx ++  tomar nx
+rota3 x =  caída de  3 x ++  tomar  3 x
 
 -- 6 Suma de n numeros elevados a cubo
 numcubos = [x*3 | x <- [1..10]]
@@ -21,14 +24,13 @@ sumaNumcubos = sum[x*3 | x <- [1..10]]
 numMayorCien = [x*x | x <- [11..2]]
 
 --8 intervalo de N numeros de 20 a 60
-nNumeros = [20,21..60]
-
+nNumeros n = [x | x <- [20..n], x >= 20, x <=60]
+ 
 --9 calcular hipotenusa con variable double
 hipotenusa ::Double-> Double-> Double
 hipotenusa a b = sqrt (a^2 + b^2)
 
 --10 Crear un programa que por medio de recursión calcule la suma de los cuadrados de n números.
-
 sumacuadrado::Int->Int
 sumacuadrado 0=0
 sumacuadrado n = n^2 + sumacuadrado(n-1)
